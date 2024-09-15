bed165dc1911ac8369ee74465edc862e73e0213e
Dhruv Mishra <mishradhruv709@gmail.com>
2024-09-16 00:08:26 +0530

    first commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+import Admin from "./pages/Admin"
+function App() {
+  return (
+    <main className="bg-white text-black">
+      <Navbar/>
+      <Admin/>
+    </main>
+  )
+}
+
+export default App
